f7112871722a01ef0c6db5ac9816f7481586407d
sumitpawar2475-beep <sumitpawar2475@gmail.com>
2026-09-10 16:07:22 +0530

    Added Best Time to Buy and Sell's Solution

--- Arrays/Best Time to Buy and Sell Stock.cpp
+++ Arrays/Best Time to Buy and Sell Stock.cpp
@@ -1,3 +1,10 @@
+// You are given an array prices where prices[i] is the price 
+// of a given stock on the ith day.
+// You want to maximize your profit by choosing a 
+// single day to buy one stock and choosing a different day in 
+// the future to sell that stock.
+
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
